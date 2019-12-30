409d2e6045e58bf36d3a554674ef3fb690f08ec9
Terran Blake <terranblake@gmail.com>
2019-12-30 11:39:11 -0800

    add husky for pre-commit hooks

---
+++ src/enums/Supported.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var Supported;
+(function (Supported) {
+    Supported["mongodb"] = "mongodb";
+    Supported["redis"] = "redis";
+    Supported["postgres"] = "postgres";
+})(Supported = exports.Supported || (exports.Supported = {}));
