bb02cfb0524641525abe36ae46d07b414f20dfe6
Terran Blake <terranblake@gmail.com>
2019-12-30 11:38:25 -0800

    add husky for pre-commit hooks

--- src/enums/Supported.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Supported;
-(function (Supported) {
-    Supported["mongodb"] = "mongodb";
-    Supported["redis"] = "redis";
-    Supported["postgres"] = "postgres";
-})(Supported = exports.Supported || (exports.Supported = {}));
--- src/mongodb.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var mongoose = require("mongoose");
-var url = process.env.MONGODB;
-mongoose.connect(url);
-mongoose.connection.on('connected', function () { return console.log('mongodb connection established'); });
